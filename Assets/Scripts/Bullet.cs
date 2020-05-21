9d2a3e95c8b1c3c6ad238b78987829024a4e912d
Chester Guinto <chesterguinto@gmail.com>
2020-05-21 15:51:57 -0700

    Added basic turret firing capabilities. - Added bullet targeting and on-hit effects.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+  private Transform target;
+
+  [Header("Bullet Characteristics")]
+  public float bulletSpeed = 70f;
+  public GameObject impactEffect;
+
+  public void Seek(Transform _target)
+  {
+    target = _target;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //If target reaches end of stage and is destroyed before bullet reaches it, destroy bullet.
+    if (target == null)
+    {
+      Destroy(gameObject);
+      return;
+    }
+
+    Vector3 dir = target.position - transform.position;
+
+    //How far our bullet will travel this frame.
+    float distanceThisFrame = bulletSpeed * Time.deltaTime;
+
+    //If our bullet will travel further than its target.
+    if (dir.magnitude <= distanceThisFrame)
+    {
+      HitTarget();
+      return;
+    }
+
+    transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+  }
+  void HitTarget()
+  {
+    Instantiate(impactEffect, transform.position, transform.rotation);
+    Destroy(gameObject);
+    Destroy(target.gameObject);
+  }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -7,10 +7,17 @@ public class Turret : MonoBehaviour
 
   private Transform target;
 
+  [Header("Attributes")]
   public float range = 15f;
   public float turnSpeed = 10f;
-  public string enemyTag = "Enemy";
+  public float fireRate = 1f;
+  private float fireCountDown = 0.5f;
+
+  [Header("Unity Setup Fields")]
   public Transform partToRotate;
+  public string enemyTag = "Enemy"; //What our turret will target.
+  public GameObject bulletPrefab;
+  public Transform firePoint;
 
   // Start is called before the first frame update
   void Start()
@@ -57,6 +64,7 @@ public class Turret : MonoBehaviour
       return;
     }
 
+    /*-------------------------Target Lock on-------------------------*/
     Vector3 dir = target.position - transform.position;
 
     //Quaternion deals with rotation, with this line we're taking our direction and giving ourselves a rotation variable to use to look that way.
@@ -68,10 +76,35 @@ public class Turret : MonoBehaviour
 
     //Since we only want to rotate along one axis, we set other rotations to 0 and take only the y component of our rotation variable.
     //Ex. if we wanted to also rotate up and down, we would take the z component of our rotation variable.
-    partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+    partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+    /*-------------------------Shooting-------------------------*/
+    if (fireCountDown <= 0f)
+    {
+      Shoot();
+      fireCountDown = 1f / fireRate;
+    }
+
+    fireCountDown -= Time.deltaTime;
   }
 
+  void Shoot()
+  {
+    //We're storing our instantiated bullet into a temporary variable so we can call the functions within it.
+    //We need variable casting when we instantiate objects and want to store them.
+    GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+    //Will grab the component called "Bullet" from our GameObject, in this case it'll grab the script.
+    Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+    if (bullet != null)
+    {
+      //Will pass on turret's target to the bullet.
+      bullet.Seek(target);
+    }
+  }
+
+  //Draws turret range for us in scene view.
   private void OnDrawGizmosSelected()
   {
     Gizmos.color = Color.red;
